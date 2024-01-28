af1e614cf7373a85d3304b228de598c7cf76a0fe
AnnikDev <a_kvernadze@cu.edu.ge>
2024-01-28 20:07:03 +0400

    css changes

--- app.js
+++ app.js
@@ -25,12 +25,16 @@ document.getElementById("menu-btn").addEventListener("click", function () {
   var sliderHeader = document.querySelector(".slider-header");
   sliderHeader.style.display =
     sliderHeader.style.display === "block" ? "none" : "block";
+  document.body.style.overflow = sliderHeader.style.display === "block" ? "hidden" : "auto";
+
 });
 
 document.getElementById("menu-close-btn").addEventListener("click", function () {
   var sliderHeader = document.querySelector(".slider-header");
   sliderHeader.style.display =
     sliderHeader.style.display === "block" ? "none" : "block";
+  document.body.style.overflow = sliderHeader.style.display === "block" ? "hidden" : "auto";
+
 });
 
 window.addEventListener("scroll", () => {
